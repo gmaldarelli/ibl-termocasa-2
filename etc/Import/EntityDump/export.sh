cbdf8c4f0a52546ad583c30170657735f5adfab8
gmaldarelli <g.maldarelli@innobitlab.it>
2024-07-16 11:24:04 +0200

    Export DB

---
+++ etc/Import/EntityDump/export.sh
@@ -0,0 +1,15 @@
+mongoexport --uri mongodb://localhost:27017 --db tpcdb2 --collection=AppBillOfMaterials --out=AppBillOfMaterials.json              
+mongoexport --uri mongodb://localhost:27017 --db tpcdb2 --collection=AppCatalogs --out=AppCatalogs.json                            
+mongoexport --uri mongodb://localhost:27017 --db tpcdb2 --collection=AppComponents --out=AppComponents.json                        
+mongoexport --uri mongodb://localhost:27017 --db tpcdb2 --collection=AppConsumptionEstimations --out=AppConsumptionEstimations.json
+mongoexport --uri mongodb://localhost:27017 --db tpcdb2 --collection=AppContacts --out=AppContacts.json                            
+mongoexport --uri mongodb://localhost:27017 --db tpcdb2 --collection=AppIndustries --out=AppIndustries.json                        
+mongoexport --uri mongodb://localhost:27017 --db tpcdb2 --collection=AppInteractions --out=AppInteractions.json                    
+mongoexport --uri mongodb://localhost:27017 --db tpcdb2 --collection=AppMaterials --out=AppMaterials.json                          
+mongoexport --uri mongodb://localhost:27017 --db tpcdb2 --collection=AppOrganizations --out=AppOrganizations.json                  
+mongoexport --uri mongodb://localhost:27017 --db tpcdb2 --collection=AppProducts --out=AppProducts.json                            
+mongoexport --uri mongodb://localhost:27017 --db tpcdb2 --collection=AppProfessionalProfiles --out=AppProfessionalProfiles.json    
+mongoexport --uri mongodb://localhost:27017 --db tpcdb2 --collection=AppQuestionTemplates --out=AppQuestionTemplates.json          
+mongoexport --uri mongodb://localhost:27017 --db tpcdb2 --collection=AppRequestForQuotations --out=AppRequestForQuotations.json    
+mongoexport --uri mongodb://localhost:27017 --db tpcdb2 --collection=AppQuotations --out=AppQuotations.json          
+            
--- src/IBLTermocasa.Blazor/Pages/Crm/Quotation.razor.cs
+++ src/IBLTermocasa.Blazor/Pages/Crm/Quotation.razor.cs
@@ -1,28 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using IBLTermocasa.BillOfMaterials;
+using IBLTermocasa.Permissions;
 using IBLTermocasa.Quotations;
 using IBLTermocasa.RequestForQuotations;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using MudBlazor;
+using Volo.Abp.AspNetCore.Components.Web.Theming.PageToolbars;
+using BreadcrumbItem = Volo.Abp.BlazoriseUI.BreadcrumbItem;
 
 namespace IBLTermocasa.Blazor.Pages.Crm;
 
 public partial class Quotation
 {
-    private QuotationDto QuotationInput = new QuotationDto();
-    public RequestForQuotationDto RfqInput = new RequestForQuotationDto();
-    private BillOfMaterialDto BillOfMaterialsInput = new BillOfMaterialDto();
+    private QuotationDto? QuotationInput = new QuotationDto();
+    public RequestForQuotationDto? RfqInput = new RequestForQuotationDto();
+    private BillOfMaterialDto? BillOfMaterialsInput = new BillOfMaterialDto();
+    private MudForm formQuotation;
+    private bool successValidationQuotation;
+    private string[] errorsValidationQuotation;
+    private bool IsLoading = true;
+    private bool CanCreateQuotation { get; set; }
+    private bool CanEditQuotation { get; set; }
+    private bool CanDeleteQuotation { get; set; }
     [Parameter] public string? Id { get; set; }
-    
+    public List<BreadcrumbItem> BreadcrumbItems { get; set; } = new();
+    public PageToolbar? Toolbar { get; set; } = new PageToolbar();
+
     protected override async Task OnInitializedAsync()
     {
-        await base.OnInitializedAsync();
+        await SetBreadcrumbItemsAsync();
+        await SetPermissionsAsync();
     }
     
     protected override async Task OnParametersSetAsync()
     {
-        
         if(Id != null && Guid.TryParse(Id, out _) && Id != Guid.Empty.ToString())
         {
             var quotation = await QuotationsAppService.GetAsync(Guid.Parse(Id));
@@ -32,11 +47,33 @@ public partial class Quotation
                 RfqInput = await RequestForQuotationsAppService.GetAsync(quotation.IdRFQ);
                 BillOfMaterialsInput = await BillOfMaterialsAppService.GetAsync(quotation.IdBOM);
             }
+            IsLoading = false;
         }
     }
 
     private void OnSentQuotation(MouseEventArgs obj)
     {
         throw new NotImplementedException();
+    }
+    
+    private async Task SetPermissionsAsync()
+    {
+        CanCreateQuotation = await AuthorizationService
+            .IsGrantedAsync(IBLTermocasaPermissions.Quotations.Create);
+        CanEditQuotation = await AuthorizationService
+            .IsGrantedAsync(IBLTermocasaPermissions.Quotations.Edit);
+        CanDeleteQuotation = await AuthorizationService
+            .IsGrantedAsync(IBLTermocasaPermissions.Quotations.Delete);
+    }
+
+    protected virtual ValueTask SetBreadcrumbItemsAsync()
+    {
+        BreadcrumbItems.Add(new BreadcrumbItem(L["Menu:Quotations"], "/quotations"));
+        if (QuotationInput != null)
+        {
+            BreadcrumbItems.Add(new BreadcrumbItem($"{L["Menu:Quotation"]} - {QuotationInput.Code} ", $"/quotation/{QuotationInput.Id}"));
+
+        }
+        return ValueTask.CompletedTask;
     }
 }
